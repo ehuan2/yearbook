a61c66431584d9667da30f2e26ea9dfb74839968
ehuan2 <eric.haoran.huang@gmail.com>
2020-05-31 10:40:43 -0400

    Finishing up the image processing

--- image_processing_run.py
+++ image_processing_run.py
@@ -1,9 +1,18 @@
 from yearbook.image_processing.utils import create_images
-from PIL import Image, ImageDraw, ImageFont
+import time
 
 if __name__ == '__main__':
-    messages = [('fdsafdsafsadfsdfsdfsdsdfsd nope test1 test2', 'from me')]
 
-    [('hello test test stestsetestsetse tsetsetsethellohello stetsetset testsetse tsetsetsetsets setsetsetsetsetset', 'from eric')]
+    start = time.perf_counter()
+    
+    messages = [('hello test test stestsetestsetse tsetsetsethellohello stetsetset\nFrom,\nEric', 'eric'), 
+    ('fdsafdsafsadfsdfsdfsdsdfsd nope test1 test2', 'me'), ('more examples from me :) are coming now', 'hello'), 
+    ('why not? Because I said so lol', 'here'), ('okay okay okay okay okay hehehehe hehehehe', 'now'), 
+    ('test test test, test, test, test', 'yes')]
+    
 
     create_images(texts = messages)
+
+    finish = time.perf_counter()
+
+    print(f'Algorithm took: {finish - start} second(s)\n')
--- yearbook/image_processing/utils.py
+++ yearbook/image_processing/utils.py
@@ -1,35 +1,46 @@
 from PIL import Image, ImageDraw, ImageFont
+import concurrent.futures
 import random
 
 # this file will create the necessary images, and then return that image based on a list of texts
-# has all the helper methods 
+# has all the helper methods
 
 # texts is a list that contains both messages and authors
-def create_images(texts = []): 
+def create_images(texts=[]):
 
-    images = []
-
-    size = (1048,1048)
-
-    for msg, author in texts:
-
-        txt = Image.new('RGBA', size, (255,255,255,255))
-
-        fnt = ImageFont.truetype('yearbook/fonts/Cherolina.ttf', 40)
-
-        d = ImageDraw.Draw(txt)
-        # d.text((0, 10), msg, font=fnt, fill=(random.randint(0,255),random.randint(0,255),random.randint(0,255),255))
-        # d.multiline_text((0, 10), msg, font=fnt, fill=(random.randint(0,255),random.randint(0,255),random.randint(0,255),255))
-        # txt.save(f'yearbook/modified/wrote_message{author}.png')
+    if len(texts) < 10: # if the data is small enough, does it synchronously
         
-        string = find_best_string(message = msg.split(' '), font = fnt, d = d)
+        for text in texts:
+            process_images(text)
+    
+    else: # does it asynchronously if the data pool is bigger
+        
+        # parallel processing version, better when handling larger quantities of data
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.map(process_images, texts)
 
-        print(string)
-        print(d.multiline_textsize(string, font=fnt))
-        print()
 
+# do processing of images here now, here is the method to call:
+def process_images(messages=[]):
 
-    return images
+    size = (1024, 1024) # default size 
+
+    msg, author = messages # gets the message and author here
+
+    txt = Image.new('RGBA', size, (255, 255, 255, 255))
+    fnt = ImageFont.truetype('yearbook/fonts/Orange_Juice.ttf', 40)
+
+    d = ImageDraw.Draw(txt)
+
+    # gets the message
+    message = find_best_string(message=msg.split(' '), pos = 0, font=fnt, d=d, size_x = size[0]/4, size_y = size[1]/4, memo = {})
+
+    if message == 'Could not resize it!':
+        return None
+
+    d.multiline_text((0, 10), message, font=fnt, fill=(random.randint(0,255),random.randint(0,255),random.randint(0,255),255))
+
+    txt.save(f'yearbook/modified/wrote_message{author}.png')
 
 
 # Steps for DP:
@@ -38,62 +49,47 @@ def create_images(texts = []):
 # 3. Relate Subproblems, min(area of one, vs area of another)
 # 4. Memoize or Tabulate
 # 5. Return Orig. Problem
+def find_best_string(message=[], pos=0, font=None, d=None, size_x=300, size_y = 300, memo = {}):
 
-# creating a method that returns the area
-def find_area(area : tuple) -> int:
-    return area[0] * area[1]
+     # if it is already memoized, it goes here
+    if memo.get(pos):
+        return memo.get(pos)
 
-counter = 0
+    # it's -1 and not len(message) because we don't care if the last one has a new line, 1/2 the time
+    if pos == len(message) - 1:
+        memo[pos] = message[pos]
+        return message[pos]
 
-# we need a method that returns the best dimensions for the text, we need to determine when to add in a new line
-# will need to take use of a dictionary and memoization
-def find_best_string(message = [], pos = 0, font = None, d = None, size_restrictions = 311, memoize = {}):
+    # creates both possibilities
+    answers = [f'''{message[pos]}\n''' + find_best_string(message=message, 
+    pos=pos+1, font=font, d=d, memo = memo),
+            f'''{message[pos]} ''' + find_best_string(message=message, 
+    pos=pos+1, font=font, d=d, memo = memo)]
 
-    # print(message)
-    # print()
-    global counter 
-    counter += 1
-    # print(counter)
-
-    if pos == len(message):
-
-        entire_string = ''''''
-
-        for msg in message:
-            entire_string += msg if "\n" in msg else msg + " "
-
-        return entire_string
-
-    answers = [find_best_string(message=message[0:pos] + [f'''{message[pos]}\n'''] + message[pos+1:len(message)], pos = pos+1, font = font, d = d, memoize=memoize),
-    find_best_string(message=message[0:pos] + [f'''{message[pos]}'''] + message[pos+1:len(message)], pos = pos+1, font = font, d = d, memoize=memoize)]
-
-    if answers[0] == 'Could not resize it!' and answers[1] == 'Could not resize it!':
-        print("Really couldn't resize it!")
-        exit()
-
-    elif answers[0] == 'Could not resize it!':
+    # if it is not resizeable for one of them, returns the other one right away
+    if answers[0] == 'Could not resize it!':
+        memo[pos] = answers[1]
         return answers[1]
 
     elif answers[1] == 'Could not resize it!':
+        memo[pos] = answers[0]
         return answers[0]
 
-    area0 = d.multiline_textsize(answers[0], font = font)
-    area1 = d.multiline_textsize(answers[1], font = font)
+    # finds the area of them, to check if it can resize
+    area0 = d.multiline_textsize(answers[0], font=font)
+    area1 = d.multiline_textsize(answers[1], font=font)
 
-    if area0[0] > size_restrictions and area1[0] > size_restrictions:
-        return "Could not resize it!"
-
-    elif area0[0] > size_restrictions:
-        memoize[pos] = answers[1]
+    # shows that it can't be resized here, if one can't, then just returns the other
+    if (area0[0] > size_x and area1[0] > size_x) or (area0[1] > size_y and area1[1] > size_y):
+        memo[pos] = 'Could not resize it!'
+        return 'Could not resize it!'
+    elif area0[0] > size_x or area0[1] > size_y: 
+        memo[pos] = answers[1]
         return answers[1]
-    elif area1[0] > size_restrictions:
-        memoize[pos] = answers[0]
+    elif area1[0] > size_x or area1[1] > size_y:
+        memo[pos] = answers[0]
         return answers[0]
-
-
-    if find_area(area0) >= find_area(area1):        
-        memoize[pos] = answers[1]
-        return answers[1]
-    else:
-        memoize[pos] = answers[0]
-        return answers[0]
+        
+    # the optimal choice is always the one with the least number of lines
+    memo[pos] = answers[1]
+    return answers[1]
